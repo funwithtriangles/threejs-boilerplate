122ce8e7e6f90e3b175d3fd609d090dbf399e194
Alex Kempton <alex@funwithtriangles.net>
2019-02-26 17:25:55 +0000

    Post prcessing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import * as THREE from 'three'
 
-import { scene, camera, renderer } from './setup'
+import { scene, camera, composer, clock, stats } from './setup'
+import { BloomEffect, EffectPass } from 'postprocessing'
 
+// Scene
 const mat = new THREE.MeshNormalMaterial()
 const geom = new THREE.TorusBufferGeometry(1, 0.3, 16, 32)
 const mesh = new THREE.Mesh(geom, mat)
-
 scene.add(mesh)
 
+// Post processing
+const effectPass = new EffectPass(camera, new BloomEffect())
+effectPass.renderToScreen = true
+composer.addPass(effectPass)
+
 const animate = () => {
+  stats.begin()
+  const delta = clock.getDelta()
+  const deltaFPS = delta * 60
   window.requestAnimationFrame(animate)
 
-  mesh.rotation.y += 0.01
+  mesh.rotation.y += 0.01 * deltaFPS
 
-  renderer.render(scene, camera)
+  composer.render(delta)
+
+  stats.end()
 }
 
 animate()
--- src/setup.js
+++ src/setup.js
@@ -1,6 +1,9 @@
 import './style.css'
 import * as THREE from 'three'
 import OrbitControlsModule from 'three-orbit-controls'
+import { EffectComposer, RenderPass } from 'postprocessing'
+import { Stats } from 'stats'
+
 const OrbitControls = OrbitControlsModule(THREE)
 
 export const renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -10,6 +13,13 @@ export const scene = new THREE.Scene()
 
 export const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000)
 camera.position.z = 5
+
+export const composer = new EffectComposer(renderer)
+composer.addPass(new RenderPass(scene, camera))
+
+export const clock = new THREE.Clock()
+
+export const stats = new Stats()
 
 const resize = () => {
   console.log(window.innerWidth)
@@ -21,6 +31,7 @@ const resize = () => {
 window.addEventListener('resize', resize)
 
 document.body.appendChild(renderer.domElement)
+document.body.appendChild(stats.dom)
 
 // eslint-disable-next-line no-new
 new OrbitControls(camera)
